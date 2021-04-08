1886204593dcd1f705d65c9dc0e1d3389edb3c7b
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2021-04-08 16:03:58 +0700

    Design UI HomeScreen

--- app/src/main/java/com/example/zendi_application/HomeScreen.java
+++ app/src/main/java/com/example/zendi_application/HomeScreen.java
@@ -1,14 +1,86 @@
 package com.example.zendi_application;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeScreen extends AppCompatActivity {
+
+    BottomNavigationView mNavigationView;
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home_screen);
+
+        mNavigationView = findViewById(R.id.bottom_nav);
+        mViewPager = findViewById(R.id.view_paper);
+
+        setUpViewPager();
+
+        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_drops:
+                        mViewPager.setCurrentItem(0);
+                        break;
+                    case R.id.action_search:
+                        mViewPager.setCurrentItem(1);
+                        break;
+                    case R.id.action_wishlist:
+                        mViewPager.setCurrentItem(2);
+                        break;
+                    case R.id.action_shop:
+                        mViewPager.setCurrentItem(3);
+                        break;
+                }
+                return true;
+            }
+        });
+
+    }
+    private void setUpViewPager(){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewPager.setAdapter(viewPagerAdapter);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        mNavigationView.getMenu().findItem(R.id.action_drops).setChecked(true);
+                        break;
+                    case 1:
+                        mNavigationView.getMenu().findItem(R.id.action_search).setChecked(true);
+                        break;
+                    case 2:
+                        mNavigationView.getMenu().findItem(R.id.action_wishlist).setChecked(true);
+                        break;
+                    case 3:
+                        mNavigationView.getMenu().findItem(R.id.action_shop).setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
