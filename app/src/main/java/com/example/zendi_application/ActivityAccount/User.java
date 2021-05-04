3db464871f7fbeed6d047b0b73b9752b52b5e725
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2021-05-04 09:36:22 +0700

    a

---
+++ app/src/main/java/com/example/zendi_application/ActivityAccount/User.java
@@ -0,0 +1,16 @@
+package com.example.zendi_application.ActivityAccount;
+
+import android.net.Uri;
+
+public class User {
+    private String Id;
+    private String Email;
+    private String Password;
+    private String Name;
+    private String DOB;
+    private String Gender;
+    private int PhoneNumber;
+    private Uri ProfilePic;
+    
+
+}
