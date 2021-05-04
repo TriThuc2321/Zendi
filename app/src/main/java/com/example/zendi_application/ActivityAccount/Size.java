c3b6e8fe4a0d831123ee4cf0a7a9da74ed4393c4
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2021-05-04 09:44:01 +0700

    user

---
+++ app/src/main/java/com/example/zendi_application/ActivityAccount/Size.java
@@ -0,0 +1,41 @@
+package com.example.zendi_application.ActivityAccount;
+
+public class Size {
+    private String Trouser;
+    private String Shoe;
+    private String Shirt;
+    private String Hat;
+
+    public String getTrouser() {
+        return Trouser;
+    }
+
+    public void setTrouser(String trouser) {
+        Trouser = trouser;
+    }
+
+    public String getShoe() {
+        return Shoe;
+    }
+
+    public void setShoe(String shoe) {
+        Shoe = shoe;
+    }
+
+    public String getShirt() {
+        return Shirt;
+    }
+
+    public void setShirt(String shirt) {
+        Shirt = shirt;
+    }
+
+    public String getHat() {
+        return Hat;
+    }
+
+    public void setHat(String hat) {
+        Hat = hat;
+    }
+
+}
--- app/src/main/java/com/example/zendi_application/ActivityAccount/User.java
+++ app/src/main/java/com/example/zendi_application/ActivityAccount/User.java
@@ -11,6 +11,81 @@ public class User {
     private String Gender;
     private int PhoneNumber;
     private Uri ProfilePic;
-    
+    private Size size;
 
+    public User(String id) {
+        Id = id;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public int getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public Uri getProfilePic() {
+        return ProfilePic;
+    }
+
+    public void setProfilePic(Uri profilePic) {
+        ProfilePic = profilePic;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
 }
