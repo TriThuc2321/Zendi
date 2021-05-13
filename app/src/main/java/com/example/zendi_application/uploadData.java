e12655739269a5813526c7f2c71957436a6da2e9
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2021-05-13 23:09:08 +0700

    choi no

--- app/src/main/java/com/example/zendi_application/uploadData.java
+++ app/src/main/java/com/example/zendi_application/uploadData.java
@@ -7,6 +7,7 @@ import android.app.Instrumentation;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,6 +21,7 @@ import android.widget.ProgressBar;
 import com.bumptech.glide.Glide;
 
 import java.io.InputStream;
+import java.net.URL;
 
 public class uploadData extends AppCompatActivity {
     private Button btnload;
@@ -70,8 +72,9 @@ public class uploadData extends AppCompatActivity {
         if(requestCode==IMAGE_REQUEST && resultCode ==RESULT_OK && data!=null && data.getData()!=null){
             Uri imageUri = data.getData();
             imgview.setImageURI(imageUri);
+            Glide.with(this).load("https://firebasestorage.googleapis.com/v0/b/zendi-1e684.appspot.com/o/testfolder%2Fimg4.jpg?alt=media&token=32f90648-76f8-4ce2-90bd-e90ef6ef052e").into(imgview1);
             //dataManager.getInstance().pushImageToStorage( "testfolder","img3",data.getData());
-            dataManager.getInstance().pushImageToStorage1(uploadData.this,"testfolder","img4",data.getData());
+            dataManager.getInstance().pushImageToStorage1(uploadData.this,"testfolder","img5",data.getData());
             }
 
     }
