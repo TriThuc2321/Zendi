9820821fdd3050657da4410a009ca0ccffe8b00c
Minhthang1506 <19522216@gm.uit.edu.vn>
2021-05-31 10:51:06 +0700

    test library

--- app/src/main/java/com/example/zendi_application/searchfragment/SearchFragment.java
+++ app/src/main/java/com/example/zendi_application/searchfragment/SearchFragment.java
@@ -9,9 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.zendi_application.R;
 import com.google.android.material.tabs.TabLayout;
@@ -19,17 +21,39 @@ import com.google.android.material.tabs.TabLayout;
 
 public class SearchFragment extends Fragment {
     private TabLayout tabLayout;
-    private ViewPager viewPager;
+    private ViewPager2 viewPager;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         tabLayout = view.findViewById(R.id.tabLayout);
         viewPager = view.findViewById(R.id.viewPager);
 
-        ViewPageAdapterForTablayout viewPageAdapterForTablayout = new ViewPageAdapterForTablayout(getChildFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        //viewPager.setAdapter(viewPageAdapterForTablayout);
-        tabLayout.setupWithViewPager(viewPager);
+        NewAdapter viewPageAdapterForTablayout = new NewAdapter(getChildFragmentManager(), getLifecycle());
+        viewPager.setAdapter(viewPageAdapterForTablayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Men"));
+        tabLayout.addTab(tabLayout.newTab().setText("Women"));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tabLayout.getSelectedTabPosition());
+            }
 
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
         return view;
     }
 
--- app/src/main/java/com/example/zendi_application/searchfragment/ViewPageAdapterForTablayout.java
+++ app/src/main/java/com/example/zendi_application/searchfragment/ViewPageAdapterForTablayout.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
 import com.example.zendi_application.searchfragment.men_fragment.MenFragment;
