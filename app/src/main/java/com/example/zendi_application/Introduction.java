9b89155dfd02392d17cb990b16979c2ae02140a6
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2021-06-05 21:15:25 +0700

    a

--- app/src/main/java/com/example/zendi_application/Introduction.java
+++ app/src/main/java/com/example/zendi_application/Introduction.java
@@ -40,7 +40,8 @@ public class Introduction extends AppCompatActivity {
         //Load data
         DataManager.LoadDropInformation("Collection/",DataManager.listDrop);  // load products
         DataManager.LoadProductInformation("Product",DataManager.listProduct); // load categors
-        DataManager.loadUser(); // load list User
+        DataManager.loadUser();
+        DataManager.GetUser(); // load list User
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
